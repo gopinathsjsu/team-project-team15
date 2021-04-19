8a194556b066ea1551e099548b09ff33165b6e71
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2021-04-19 16:23:19 -0700

    Revert "Login"
    This reverts commit d4321a3210ad1e40ae680d2d79fc8652fe0f1bc5.

--- flaskbank/deploy/__init__.py
+++
@@ -1,3 +0,0 @@
-from flaskbank.backend import create_app
-from flaskbank.backend.config import ProductionConfig
-app = create_app(ProductionConfig)
--- flaskbank/flaskbank/react-app/src/reducers/UploadCheckImage/UpLoadCheckImage.js
+++
@@ -1,8 +0,0 @@
-import * as ACTION from "../../static/action_type";
-
-export const UpLoadCheckImage = ( state= false, action) => {
-    if (action.type === ACTION.IMAGE_ON || action.type === ACTION.IMAGE_OFF) {
-        return action.payload;
-    }
-    return state;
-};
